b713ade950c612833d58d77ba8079cb4eb752db2
Cosmos-52 <hongjin_zhu@cmu.ac.th>
2023-06-30 17:34:19 +0700

    step4
    4.1-4.9

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ createApp({
         const product = ref('Boots')
         const link = ref('https://www.camt.cmu.ac.th/index.php/en/')
         const image = ref('assets/images/socks_green.jpg')
+        const onsale = ref(true)
+        const inStock = ref(true)
+        const inventory = ref(0)
         const description = ref('Description')
         return {
             product, 
             link,
             image,
+            inStock,
+            inventory,
+            onsale,
             description
         };
     }
