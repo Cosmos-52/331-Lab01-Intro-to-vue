9ecc5a510afb36d4e6d1bfb98ac0da048cb70a6d
Cosmos-52 <hongjin_zhu@cmu.ac.th>
2023-06-30 17:04:42 +0700

    step3
    3.1-3.6

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ const { createApp, ref} = Vue
 createApp({
     setup(){
         const product = ref('Boots')
+        const link = ref('https://www.camt.cmu.ac.th/index.php/en/')
+        const image = ref('assets/images/socks_green.jpg')
         const description = ref('Description')
         return {
-            product, description
+            product, 
+            link,
+            image,
+            description
         };
     }
 }).mount('#app')
