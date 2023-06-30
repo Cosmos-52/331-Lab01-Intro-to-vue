e313801c05f330344967ba9770a5278fa3270355
Cosmos-52 <hongjin_zhu@cmu.ac.th>
2023-06-30 18:02:42 +0700

    step5
    5.1-5.5

--- main.js
+++ main.js
@@ -9,13 +9,31 @@ createApp({
         const inStock = ref(true)
         const inventory = ref(0)
         const description = ref('Description')
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            {id: 2234, color: 'green'},
+            {id: 2235, color: 'blue'}
+        ])
+        const sizes = ref([
+            'S',
+            'M',
+            'L'
+        ])
+
         return {
             product, 
             link,
             image,
             inStock,
             inventory,
+            details,
             onsale,
+            variants,
+            sizes,
             description
         };
     }
