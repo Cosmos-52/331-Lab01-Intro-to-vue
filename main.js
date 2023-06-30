43d606d741ffd312f5179fabbd873524d60cb9f8
Cosmos-52 <hongjin_zhu@cmu.ac.th>
2023-06-30 10:51:11 +0700

    step2
    2.1-2.5

---
+++ main.js
@@ -0,0 +1,11 @@
+const { createApp, ref} = Vue
+
+createApp({
+    setup(){
+        const product = ref('Boots')
+        const description = ref('Description')
+        return {
+            product, description
+        };
+    }
+}).mount('#app')
